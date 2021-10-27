89b9aca4b8215f6fe835270c34a74a7113d6d589
Harsha Vardhan Pabbineedi <harsha.mvgr@gmail.com>
2021-10-28 00:04:19 +0800

    application skeleton

---
+++ OCBCDemo/OCBCDemo/AppDelegate.swift
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.swift
+//  OCBCDemo
+//
+//  Created by Pabbineedi Harsha on 27/10/21.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    var window: UIWindow?
+    var viewController: ViewController?
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        window = UIWindow(frame: UIScreen.main.bounds)
+        let storyBoardRef = UIStoryboard.init(name: "Main", bundle: nil)
+        window?.rootViewController = storyBoardRef.instantiateViewController(withIdentifier: "ViewController") as! ViewController
+        window?.makeKeyAndVisible()
+        return true
+    }
+}
+
